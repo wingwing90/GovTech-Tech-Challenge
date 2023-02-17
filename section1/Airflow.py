7a0b8fa7d7366f3225bca56c3a9a128afb62c0b3
wingwing90 <102972000+wingwing90@users.noreply.github.com>
2023-02-17 19:01:20 +0800

    Create Airflow.py

---
+++ section1/Airflow.py
@@ -0,0 +1,32 @@
+from datetime import date, datetime, timedelta
+
+from airflow import DAG
+
+version = 1
+default_args = {
+    "start_date": datetime(2023, 2, 17, 0, 0, 0),
+    "retries": 2,
+    "max_active_runs": 1,
+    "retry_delay": timedelta(minutes=1),
+}
+
+def filter_data():
+    
+
+with DAG(
+    dag_id=f"psp-isbank-fetcher-v{version}",
+    description="Fetch reports from Isbank and load to Datahub",
+    schedule_interval=" 0 * * * * ",
+    default_args={**DAG_DEFAULT_ARGS, **default_args},
+    max_active_runs=1,
+    tags=[DAG_DEFAULT_ARGS["owner"], "dwh"],
+    catchup=False,
+    on_failure_callback=alerts.setup_on_failure_callback(),
+) as dag:
+
+    FilterData = PythonOperator(
+        task_id = "FilterData",
+        python_callable = filter_data
+    )
+    
+
